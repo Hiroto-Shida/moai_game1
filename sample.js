136033fe597956b1a886ad5278e5847fd9a5182f
Hiroto-Shida <uecshida6295@gmail.com>
2020-10-28 16:50:17 +0900

    update

--- sample.js
+++ sample.js
@@ -14,22 +14,16 @@ var random_x;
 var random_y;
 var gmRect;
 
-var count_num;
-var count = 0;
+var count_num; //ゴミ箱に捨てた数(カウント)の情報
+var count = 0; //ゴミ箱に捨てた数(カウント)
 
 (function() {
     // スクロールを禁止する関数
     function noScroll(event) {
       event.preventDefault();
     }
-    // スクロール禁止(SP)
-    document.addEventListener('touchmove', noScroll, { passive: false });
-    // スクロール禁止(PC)
-    document.addEventListener('mousewheel', noScroll, { passive: false });
-    // スクロール禁止を解除(SP)
-    // document.removeEventListener('touchmove', noScroll, { passive: false });
-    // スクロール禁止を解除(PC)
-    // document.removeEventListener('mousewheel', noScroll, { passive: false });
+    document.addEventListener('touchmove', noScroll, { passive: false }); // スクロール禁止(SP)
+    document.addEventListener('mousewheel', noScroll, { passive: false }); // スクロール禁止(PC)
 })();
 
 // 初期設定
